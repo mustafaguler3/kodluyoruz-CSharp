815b7fa20f8e6e547f29d1e3205556c9d77bef8e
mustafaguler3 <mustafa.glr@hotmail.com>
2022-02-21 14:31:27 +0300

    question four

---
+++ AlgorithmProject-1/Question-4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Project
+{
+    class QestionFour
+    {
+        static void Main(string[] args)
+        {
+            
+        
+            int numberOfletter = 0;
+            System.Console.WriteLine("Enter a sentence ");
+            string sentence = Console.ReadLine();
+            string[] worb = sentence.Split(" ");
+            System.Console.WriteLine("There Are {0} Words In The Given Sentence.",worb.Length);
+            for (int i = 0; i < worb.Length; i++)
+            {
+                 char[] letterArray = worb[i].ToCharArray();
+                numberOfletter+= letterArray.Length;
+            }
+            System.Console.WriteLine("There Are {0} Letters in the Given Sentence.",numberOfletter);
+
+
+
+           
+
+
+
+
+
+        }
+    }
+}

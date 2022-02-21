5d042cc3e1bcf86f017897146dc2480131fa4148
mustafaguler3 <mustafa.glr@hotmail.com>
2022-02-21 09:55:15 +0300

    exception handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace hatayonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //try
+           //{
+           //    Console.WriteLine("bir sayi giriniz:");
+           //    int sayi = Convert.ToInt32(Console.ReadLine());
+           //    Console.WriteLine("girmiş olduğunuz sayı:"+sayi);
+           //}
+           //catch(Exception ex)
+           //{
+           //    Console.WriteLine("Hata:"+ex.Message.ToString());
+           //}
+           //finally
+           //{
+           //    Console.WriteLine("işlem tamamlandı.");
+           //}
+           
+           try
+           {
+               //int a = int.Parse(null);
+               //int a = int.Parse("test");
+               int a = int.Parse("-20000000000");
+           }
+           catch (ArgumentNullException ex)
+           {
+               Console.WriteLine("boş değer girdiniz");
+               Console.WriteLine(ex);
+           }
+           catch (FormatException ex)
+           {
+               Console.WriteLine("veri tipi uygun degil");
+               Console.WriteLine(ex);
+           }    
+           catch (OverflowException ex)
+           {
+               Console.WriteLine("çok kücük veya cok büyük deger girdiniz");
+               Console.WriteLine(ex);
+           }
+           finally
+           {
+               Console.WriteLine("işlem başarı ile tamamlandı");
+           }
+           
+
+        }
+    }
+}

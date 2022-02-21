e7636996856161f6cc9ab16dc183cf23771d6d6f
mustafaguler3 <mustafa.glr@hotmail.com>
2022-02-21 14:23:30 +0300

    question two

--- AlgorithmProject-1/Question-2.cs
+++ AlgorithmProject-1/Question-2.cs
@@ -1,1 +1,44 @@
-﻿
+﻿using System;
+
+namespace Project
+{
+    class QuestionTwo
+    {
+        static void Main(string[] args)
+        {
+            
+            System.Console.WriteLine("Enter N number");
+            int nNumber= int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Enter M number");
+            int mNumber = int.Parse(Console.ReadLine());
+            int[] array = new int[nNumber];
+            for (int i = 0; i < array.Length; i++)
+            {
+                System.Console.WriteLine("Enter {0}. number ",i+1);
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i]%mNumber == 0)
+                    System.Console.WriteLine(array[i]+" is able to divided by M number");
+                else if (array[i]==mNumber)
+                {
+                    System.Console.WriteLine(array[i]+" is equal to M number");
+                }
+                else
+                {
+                    System.Console.WriteLine(array[i]+" is not able to divided by M number");
+                }
+            }
+
+
+
+           
+
+
+
+
+
+        }
+    }
+}
